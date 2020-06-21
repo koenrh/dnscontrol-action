5df73401c5b347a9c9f74554263309c503a67b21
Sven Luijten <svenluijten1995@gmail.com>
2020-06-21 19:46:35 +0200

    Output the "dnscontrol" output in the Action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,13 @@
 
 set -eo pipefail
 
-sh -c "dnscontrol $*"
+OUTPUT=`sh -c "dnscontrol $1"`
+
+echo "$OUTPUT"
+
+# https://github.community/t/set-output-truncates-multiline-strings/16852/3
+OUTPUT="${OUTPUT//'%'/'%25'}"
+OUTPUT="${OUTPUT//$'\n'/'%0A'}"
+OUTPUT="${OUTPUT//$'\r'/'%0D'}"
+
+echo "::set-output name=output::$OUTPUT"
