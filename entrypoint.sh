229fdd3a1c47491fbeec660c1ad5c0eff97da7ae
Koen Rouwhorst <info@koenrouwhorst.nl>
2021-01-16 18:22:27 +0100

    Remove DNSControl prefix from input arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,10 @@
 set -eo pipefail
 
 # Resolve to full paths
-CONFIG_ABS_PATH="$(readlink -f "${INPUT_DNSCONTROL_CONFIG_FILE}")"
-CREDS_ABS_PATH="$(readlink -f "${INPUT_DNSCONTROL_CREDS_FILE}")"
-
+CONFIG_ABS_PATH="$(readlink -f "${INPUT_CONFIG_FILE}")"
+CREDS_ABS_PATH="$(readlink -f "${INPUT_CREDS_FILE}")"
 
 WORKING_DIR="$(dirname "${CONFIG_ABS_PATH}")"
-#CONFIG_FILE="$(basename "${INPUT_DNSCONTROL_CONFIG_FILE}")"
 cd "$WORKING_DIR"
 
 IFS=
